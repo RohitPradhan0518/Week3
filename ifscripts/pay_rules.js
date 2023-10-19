38007a6b2af1b66cbed96ab9a2f231f9d76bc7f0
Rohitpradhan <Rohitpradhan0518@gmail.com>
2023-10-19 11:29:16 -0500

    payroll

---
+++ ifscripts/pay_rules.js
@@ -0,0 +1,19 @@
+// INPUT PARAMETERS
+const payRate = 17.30
+const hoursWorked = 45
+
+// OUTPUT RETURNED
+let pay = 0
+
+//IS REGULAR?
+if(hoursWorked <= 40){
+    pay = payRate * hoursWorked;
+}else{
+    standardPay = payRate * 40;
+    overtimeHours = hoursWorked - 40
+    overtimeRate = payRate * 1.5
+    overTimePay = overtimeHours * overtimeRate
+    pay = standardPay + overTimePay
+}
+
+console.log(pay)
