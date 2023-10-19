ebe35f7a8f3a6eb8ce768b40c6a41c093a7be04b
Rohitpradhan <Rohitpradhan0518@gmail.com>
2023-10-19 14:36:51 -0500

    update

---
+++ ifscripts/gregorian_calendar.js
@@ -0,0 +1,8 @@
+year = 1900
+ //three conditions to find out the leap year
+ if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+    console.log(year + ' is a leap year');
+} else {
+    console.log(year + ' is not a leap year');
+}
+
